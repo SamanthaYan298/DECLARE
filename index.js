e93f3fe74233edabaa3d9e0a045d8cec0f30c485
Yisha Yan <yisha-y@hotmail.com>
2023-09-17 14:28:16 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+// Delcare
+
+
+/**Question 1 
+ * Declare the variable computer
+*/
+
+var computer;
+
+/** Question 2 
+ * Declare the variable pen
+*/
+
+var pen;
+
+/** Question 3 
+ * Declare the variable paper bag
+*/
+
+var paperBag;
+
+/** Question 4 
+ * Declare the variable outdoor activties
+*/
+
+var outdoorActivities;
